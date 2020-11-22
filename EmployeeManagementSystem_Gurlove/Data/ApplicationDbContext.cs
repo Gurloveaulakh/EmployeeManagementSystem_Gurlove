b81c9111ab54af7cb706426fdefc867b3c48e3a0
singhnirmal128@gmail.com <singhnirmal128@gmail.com>
2020-11-22 23:45:52 +0530

    Add project files.

---
+++ EmployeeManagementSystem_Gurlove/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeManagementSystem_Gurlove.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystem_Gurlove.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Designation> Designations { get; set; }
+        public DbSet<EmployeeDetail> EmployeeDetails { get; set; }
+    }
+}
---
+++ EmployeeManagementSystem_Gurlove/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeManagementSystem_Gurlove.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
